6286196fecec2282aa8dbb001cd3d0b85849738d
ClassCat <support@classcat.com>
2015-05-06 15:18:36 +0900

    assets/cc-init.sh : add init code, change a var name.

--- assets/cc-init.sh
+++ assets/cc-init.sh
@@ -2,21 +2,50 @@
 
 ########################################################################
 # ClassCat/Dovecot Asset files
-# maintainer: Masashi Okumura < masao@classcat.com >
+# Copyright (C) 2015 ClassCat Co.,Ltd. All rights reserved.
 ########################################################################
 
-### HISTORY ###
+#--- HISTORY -----------------------------------------------------------
+# 06-may-15 : Add init code.
+# 06-may-15 : Change var name 'users' to 'USERS'.
 # 04-may-15 : Add sshd and code portion to handle root password.
 # 03-may-15 : Removed the nodaemon steps.
-#
+#-----------------------------------------------------------------------
+
+
+######################
+### INITIALIZATION ###
+######################
+
+function init () {
+  echo "ClassCat Info >> initialization code for ClassCat/Dovecot"
+  echo "Copyright (C) 2015 ClassCat Co.,Ltd. All rights reserved."
+  echo ""
+}
+
 
 ############
 ### SSHD ###
 ############
 
 function change_root_password() {
-  echo -e "root:${password}" | chpasswd
-  # echo -e "${password}\n${password}" | passwd root
+  if [ -z "${ROOT_PASSWORD}" ]; then
+    echo "ClassCat Warning >> No ROOT_PASSWORD specified."
+  else
+    echo -e "root:${ROOT_PASSWORD}" | chpasswd
+    # echo -e "${password}\n${password}" | passwd root
+  fi
+}
+
+
+function put_public_key() {
+  if [ -z "$SSH_PUBLIC_KEY" ]; then
+    echo "ClassCat Warning >> No SSH_PUBLIC_KEY specified."
+  else
+    mkdir -p /root/.ssh
+    chmod 0700 /root/.ssh
+    echo "${SSH_PUBLIC_KEY}" > /root/.ssh/authorized_keys
+  fi
 }
 
 
@@ -29,7 +58,7 @@ function config_dovecot () {
 }
 
 function add_accounts () {
-  echo $users | tr , \\n > /var/tmp/users
+  echo ${USERS} | tr , \\n > /var/tmp/users
   while IFS=':' read -r _user _id _pwd; do
     useradd -u $_id -s /usr/sbin/nologin -m $_user
     echo -e "${_pwd}\n${_pwd}" | passwd $_user
@@ -42,9 +71,13 @@ function proc_dovecot () {
   add_accounts
 }
 
+
 ##################
 ### SUPERVISOR ###
+##################
+
 # See http://docs.docker.com/articles/using_supervisord/
+
 function proc_supervisor () {
   cat > /etc/supervisor/conf.d/supervisord.conf <<EOF
 [program:dovecot]
@@ -54,7 +87,7 @@ command=/opt/cc-dovecot.sh
 command=/usr/sbin/sshd -D
 
 [program:rsyslog]
-command=/usr/sbin/rsyslogd -n -c3
+command=/usr/sbin/rsyslogd -n
 EOF
 
   cat >> /opt/cc-dovecot.sh <<EOF
@@ -67,11 +100,16 @@ EOF
 }
 
 
+init
 change_root_password
+put_public_key
 proc_dovecot
 proc_supervisor
 
 # /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 
 exit 0
+
+
+### End of Script ###
 
